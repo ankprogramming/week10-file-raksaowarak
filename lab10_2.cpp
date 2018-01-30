901bae115a67b68dac7df723ce74e750f662be77
Unknown <lovechillyhot@hotmail.com>
2018-01-30 11:04:18 +0700

    เสร็จข้อที่ 2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
 int main() {
+    string Line;
+    ifstream myfile ("example.txt");
+    if (myfile.is_open())
+    {
+        while(getline (myfile,Line))
+        {
+            cout <<Line << '\n';
+        }
+        myfile.close();
+    }
+    else cout << "unable to open file";
         /* code */
         return 0;
 }
